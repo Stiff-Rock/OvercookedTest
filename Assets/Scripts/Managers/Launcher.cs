b972310cae96a8293bf8d4ceba14cea02f576401
StiffRock <yaguitor@gmail.com>
2025-12-17 11:47:24 +0100

    Instace

--- Assets/Scripts/Managers/Launcher.cs
+++ Assets/Scripts/Managers/Launcher.cs
@@ -1,11 +1,15 @@
 using Unity.Netcode;
 using Unity.Netcode.Transports.UTP;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(NetworkManager))]
 [RequireComponent(typeof(UnityTransport))]
 public class Launcher : MonoBehaviour
 {
+    [SerializeField] private SceneAsset gameScene;
+
     private void Start()
     {
         NetworkManager.Singleton.OnClientConnectedCallback += OnClientConnected;
@@ -24,11 +28,20 @@ public class Launcher : MonoBehaviour
 
     private void OnClientConnected(ulong clientId)
     {
+        if (!NetworkManager.Singleton.IsServer)
+            return;
+
         Debug.Log($"Cliente conectado con Id {clientId}");
+
+        if (SceneManager.GetActiveScene().name == gameScene.name)
+        {
+            NetworkManager.Singleton.SceneManager.LoadScene(gameScene.name, LoadSceneMode.Single);
+        }
     }
 
     private void OnServerStarted()
     {
         Debug.Log($"Servidor iniciado");
+        NetworkManager.Singleton.SceneManager.LoadScene(gameScene.name, LoadSceneMode.Single);
     }
 }
